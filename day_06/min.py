d5d43de4752dd9a3f2ff4fd29c42f994b3d5ee08
Alex Wilson <alex.david.wilson@gmail.com>
2021-12-06 17:59:52 -0700

    even more day 6 minification to include parts 1 and 2 in one

--- day_06/min.py
+++ day_06/min.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-for days in [80, 256]:
-    ages = [0]*9
-    for i in open('input.txt').read().split(','):
-        ages[int(i)] += 1
+ages = [0]*9
+for i in open('input.txt').read().split(','):
+    ages[int(i)] += 1
 
-    for _ in [0]*days:
-        ages = ages[1:] + ages[:1]
-        ages[6] += ages[8]
-    print(sum(ages))
+for d in range(256):
+    ages = ages[1:] + ages[:1]
+    ages[6] += ages[8]
+    d in [79, 255] and print(sum(ages))
