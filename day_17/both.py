d9b23c709b10e2e43892a7e9f88b9a84f73b76e8
Alex Wilson <alex.david.wilson@gmail.com>
2021-12-17 23:35:20 -0700

    add day 17 z3 musings

--- day_17/both.py
+++ day_17/both.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import pprint as pp
+from z3 import *
 
 def parse(input):
     lines = [l.strip() for l in open(input)]
@@ -83,8 +84,56 @@ def part2(input, debug=False):
     print(f'Part 2 {input}: {len(uniq) = }')
 
 
+def part2_z3(input, debug=False):
+    x_min, x_max, y_min, y_max = parse(input)
+    if debug: print(x_min, x_max, y_min, y_max)
+
+    s = Optimize()
+
+    xv = Int('xv')
+    x = Int('x')
+    x_drag = Int('x_drag')
+    yv = Int('yv')
+    y = Int('y')
+    t = Int('t')
+
+    s.add(t > 0)
+    s.add(And(xv >= 0, xv < 500))
+    s.add(And(yv > -500, yv < 500))
+    s.add(x_drag == (t*(t+1))/2)
+    s.add(x == If(t < xv, xv * t - x_drag, (xv*(xv+1))/2))
+    s.add(y == yv * t - (t * (t + 1) / 2))
+
+    s.add(And(x >= x_min, x <= x_max))
+    s.add(And(y >= y_min, y <= y_max))
+
+    print(s)
+    
+    grid = {}
+    for xi in range(x_min, x_max+1):
+        for yi in range(y_min, y_max+1):
+            grid[(xi,yi)] = True
+
+    passing_cnt = 0
+    while True:
+        ret = s.check()
+        if ret == sat:
+            m = s.model()
+            passing, _ = shoot(m[xv].as_long(), m[yv].as_long(), grid, y_min, True)
+            if passing: passing_cnt += 1
+            print(m)
+            print(f'{m[xv]} {m[yv]} -> ({m[x]}, {m[y]}) {passing = } {passing_cnt = }')
+            print()
+            s.add(Or(xv != m[xv], yv != m[yv]))
+        else:
+            print(ret)
+            break
+
+
 part1('example.txt')
 part1('input.txt')
 
 part2('example.txt')
 part2('input.txt')
+
+# part2_z3('example.txt')
